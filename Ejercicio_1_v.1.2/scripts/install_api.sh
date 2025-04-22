020b06a6e255de826e4686f7e1979afe3302d854
abeladrian-garcia <abeladrian.garcia.external@evolutio.com>
2025-04-22 16:12:35 -0300

    agrego script

--- Ejercicio_1_v.1.2/scripts/install_api.sh
+++ Ejercicio_1_v.1.2/scripts/install_api.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-apt update -y && apt install -y nfs-common python3 python3-pip
+apt update -y && apt install -y nfs-common python3 python3-pip  #ejemplo instalar dependencias
 
 # Montar EFS
 mkdir -p /mnt/efs
-mount -t nfs4 -o nfsvers=4.1 fs-XXXXXXX.efs.us-east-1.amazonaws.com:/ /mnt/efs
+mount -t nfs4 -o nfsvers=4.1 fs-qwert1234.efs.us-east-1.amazonaws.com:/ /mnt/efs    #ejemplo montar efs
 
 # Descargar el código
-git clone https://github.com/tuusuario/api-rest-efs.git /opt/api
-cd /opt/api/api
+git clone git@github.com:adrianabelgarcia/meli_challenge.git /opt/api      #ejemplo clonar repositorio                  
 
 # Ejecutar la API
-nohup python3 server.py &
+nohup python3 server.py &    #ejemplo ejecutar api
--- Ejercicio_1_v.1.2/scripts/install_haproxy.sh
+++ Ejercicio_1_v.1.2/scripts/install_haproxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt update -y && apt install -y haproxy
+apt update -y && apt install -y haproxy #instalo dependencias
 
 # Copiar config
-cp /opt/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg
-systemctl restart haproxy
+cp /opt/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg    #copio config
+systemctl restart haproxy    #reinicio servicio
